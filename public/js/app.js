265cb7364e77ff462f522bd001e169e1f3c01cb6
Jason C Lawrence <jasonclawrence1991@gmail.com>
2020-05-04 10:29:01 -0500

    Update B2B No Reload

--- public/js/app.js
+++ public/js/app.js
@@ -49415,7 +49415,7 @@ __webpack_require__.r(__webpack_exports__);
         .post("/store-settlement-broker", this.settlement_account)
         .then(response => {
           this.getTradingAccountsList();
-           setTimeout(location.reload.bind(location), 1000);
+          //  setTimeout(location.reload.bind(location), 1000);
           this.create = false;
         })
         .catch(error => {});
--- resources/js/components/B2b.vue
+++ resources/js/components/B2b.vue
@@ -346,7 +346,7 @@ export default {
         .post("/store-settlement-broker", this.settlement_account)
         .then(response => {
           this.getTradingAccountsList();
-           setTimeout(location.reload.bind(location), 1000);
+          //  setTimeout(location.reload.bind(location), 1000);
           this.create = false;
         })
         .catch(error => {});
