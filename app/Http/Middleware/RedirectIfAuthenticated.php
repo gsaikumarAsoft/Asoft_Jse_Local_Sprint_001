4560a0ac78f530644bd239ccb28d4a90011ba870
Jason C Lawrence <jasonclawrence1991@gmail.com>
2020-05-06 23:39:27 -0500

    Redirect Patch

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use App\Providers\RouteServiceProvider;
 use Closure;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
 
 class RedirectIfAuthenticated
@@ -19,7 +20,8 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect(RouteServiceProvider::HOME);
+            // return redirect(RouteServiceProvider::HOME);
+            return new RedirectResponse(url('/'));
         }
 
         return $next($request);
