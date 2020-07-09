b7372964c00dbbd092d07f3b250f8cfe023caf57
Jason C Lawrence <jasonclawrence1991@gmail.com>
2020-07-09 13:36:09 -0500

    Reload

--- public/js/app.js
+++ public/js/app.js
@@ -73595,10 +73595,10 @@ __webpack_require__.r(__webpack_exports__);
         if (valid) {
           console.log(data);
           this.$swal(data.errors);
-          // this.reloadPage();
+          this.reloadPage();
         } else {
           this.$swal(data.errors);
-          // this.reloadPage();
+          this.reloadPage();
         }
 
         // })
