73669415fdf3ea2a0ad746ba99063c41104b3a74
Jason C Lawrence <jasonclawrence1991@gmail.com>
2020-09-02 08:19:40 -0500

    Pre Select Order TYpe And Handling Instructions For Broker Admin

--- resources/js/components/brokers/Orders.vue
+++ resources/js/components/brokers/Orders.vue
@@ -127,7 +127,7 @@
                       v-model="order.market_order_number"
                       type="text"
                       placeholder="Enter Market Order Number"
-                      :disabled="disabled"
+                      disabled
                     ></b-form-input>
                   </b-form-group>
                 </b-col>
@@ -1131,6 +1131,10 @@ export default {
       this.disabled = false;
       var dt = new Date();
       this.order = {};
+
+      this.order.order_type = this.order_types[0]; //Preselect the order type by default
+      this.order.handling_instructions = this.handling_options[0];
+      console.log(this.order_types[0]);
       // The “OrderID” must be unique per request submitted.
       this.order.client_order_number =
         formatteddatestr + ("" + Math.random()).substring(2, 5);
